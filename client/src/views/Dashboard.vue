5b36dccc4a4b3dacfb5b9577f6e1a5bc1e9cb594
Rami <Alhusseinr1@montclair.edu>
2020-02-11 18:59:44 -0500

    adding a loging and dashboard view

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="dashboard">
+        <p>
+            this is the dashboard
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'dashboard',
+        data(){
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="LoginForm">
+        <h1>Login</h1>
+        <input type="number" name="pin"  v-model="waiter.pin" placeholder="pin" />
+        <button type="button" v-on:click="login()">Login</button>
+    </div>
+</template>
+
+<script>
+ export default {
+    name: 'log-in form',
+     data(){
+        return{
+            submitting: false,
+            success: false,
+            error: false,
+            waiter:{
+                pin: ''
+            }
+        }
+     },
+     methods: {
+        handleSubmit(){
+            this.submitting = true;
+            this.clearStatus();
+
+            if(this.invalidPin){
+                this.error = true;
+                return;
+            }
+
+        },
+         clearStatus(){
+            this.error = false;
+            this.success = false;
+         },
+         login(){
+            if(this.waiter.pin != ''){
+                if(this.waiter.pin == this.$parent.mockAccount)
+            }
+         }
+     },
+     computed:{
+        invalidPin(){
+            return this.waiter.pin === ''
+        }
+     }
+ }
+</script>
+
+<style scoped>
+
+</style>
